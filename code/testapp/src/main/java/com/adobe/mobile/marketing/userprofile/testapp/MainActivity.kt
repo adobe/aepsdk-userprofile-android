c91bc429f71472a573051edc5320312601b16e68
yansong <yanyang@adobe.com>
2024-07-11 18:00:46 -0500

    update test app

--- code/testapp/src/main/java/com/adobe/mobile/marketing/userprofile/testapp/MainActivity.kt
+++ code/testapp/src/main/java/com/adobe/mobile/marketing/userprofile/testapp/MainActivity.kt
@@ -14,16 +14,23 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.adobe.marketing.mobile.MobileCore
 import com.adobe.marketing.mobile.UserProfile
 import com.adobe.mobile.marketing.userprofile.testapp.ui.theme.AepsdkuserprofileandroidTheme
 
@@ -59,6 +66,7 @@ fun Input() {
     val getterResultState = remember {
         mutableStateOf(false)
     }
+    var appId by remember { mutableStateOf("your-appId") }
     Column(Modifier.padding(8.dp)) {
         Row {
 
@@ -148,6 +156,32 @@ fun Input() {
                     }
                 )
             }
+        }
+        Surface(
+            border = BorderStroke(1.dp, Color.Gray),
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(PaddingValues(8.dp)),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                OutlinedTextField(
+                    value = appId,
+                    onValueChange = { appId = it },
+                    label = { Text("appId") }
+                )
+                Button(onClick = {
+                    MobileCore.configureWithAppID(appId)
+                }) {
+                    Text(text = "configureWithAppID(\"appId\")")
+                }
+            }
+        }
+        Button(onClick = {
+            MobileCore.trackAction("action", null)
+        }) {
+            Text(text = "trigger rule consequence(update profile)")
         }
     }
 }
